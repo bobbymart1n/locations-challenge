74e157535ad261966a8e5a65bcadbe20d75cda07
Bobby Martin and Luke Stockburger <luke@>
2018-04-02 11:19:11 -0700

    add initial files, html elements, business logic keys

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ A OO Project for Epicodus 04.02.2018
 # Description
 
 # Specifications
-
-
+|Spec|Input|Output|
+| --- | --- | --- |
+|Program will log a place the user has been | Egypt | Egypt |
 # Technologies
---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//business logic
+function Location(place, date, landmark, climate) {
+  this.placeName = place;
+  this.timeOfYear = date;
+  this.landmark = landmark;
+  this.weather = climate;
+
+}
+//user interface logic
+$(document).ready(function () {
+  $("#").submit(function(event) {
+    event.preventDefault();
+
+  });
+});
