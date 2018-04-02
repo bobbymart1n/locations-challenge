797a6a558ef825c07109ac8b286c3cc36bc7963f
Bobby Martin and Luke Stockburger <bobby@>
2018-04-02 11:56:56 -0700

    add <p> tags to show input/output, add more user logic

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,22 @@ function Location(place, date, landmark, climate) {
 }
 //user interface logic
 $(document).ready(function () {
-  $("#").submit(function(event) {
+  $("#place").submit(function(event) {
     event.preventDefault();
+    var inputtedPlace = $("#locationName").val();
+    var inputtedDate = $("#date").val();
+    var inputtedLandmark = $("#landmark").val();
+    var inputtedClimate = $("#climate").val();
 
+    var newLocation = new Location(inputtedPlace, inputtedDate, inputtedLandmark, inputtedClimate);
+    $("#output ul").append("<li><span class='location'>" + newLocation.placeName +"</span></li>")
+
+    $("#output").last().click(function() {
+      $("#show-location").show();
+      $("#show-location h2").text(newLocation.placeName);
+      $(".date").text(newLocation.timeOfYear);
+      $(".landmark").text(newLocation.landmark);
+      $(".climate").text(newLocation.weather);
+    });
   });
 });
